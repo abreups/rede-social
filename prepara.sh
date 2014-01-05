6c6a171fe36bd85e80c1e86b123e1df101c3eb40
Paulo Abreu <abreups@gmail.com>
2014-01-05 11:46:28 -0200

    v2

--- prepara.sh
+++ prepara.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install g++ curl libssl-dev apache2-utils make
+sudo apt-get install -y g++ curl libssl-dev apache2-utils make
 wget http://nodejs.org/dist/v0.10.24/node-v0.10.24.tar.gz
 tar -xf node-v0.10.24.tar.gz
 cd node-v0.10.24
