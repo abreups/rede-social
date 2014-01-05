e851b620155581938aaa61775c7333a6ce8d49ac
Paulo S Abreu <abreups@gmail.com>
2014-01-05 13:42:44 +0000

    adicionado v1 do script de preparacao do ambiente

---
+++ prepara.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install g++ curl libssl-dev apache2-utils make
+wget http://nodejs.org/dist/v0.10.24/node-v0.10.24.tar.gz
+tar -xf node-v0.10.24.tar.gz
+cd node-v0.10.24
+./configure
+make
+make install
+which node
+node -v
+curl http://npmjs.org/install.sh | sh
+which npm
+npm -v
+
