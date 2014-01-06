9e291a93252201454584ace969c0807e0049ee35
Paulo Santos Abreu <abreups@gmail.com>
2014-01-06 14:14:54 -0200

    Account.js completa - first commit

--- models/Account.js
+++ models/Account.js
@@ -27,12 +27,17 @@ module.exports = function(config, mongoose, nodemailer) {
 	};
 
 	var changePassword = function(accountId, newpassword) {
-		var shaSum = crypto.createHash('sha256');
-		shaSum.update(newpassword);
-		var hashedPassword = shaSum.digest('hex');
+		var shaSum = crypto.createHash('sha256'); // cria um hash criptográfico usando o algoritmo 'sha256'
+		shaSum.update(newpassword); // updates the hash content with the given data (i.e. newpassword)
+		var hashedPassword = shaSum.digest('hex'); // essa operação é: newpassword -> hash() -> digest, e o digest é armazenado
 		Account.update(
 			{_id:accountId}, 
+			// O comando $set do MongoDB altera um único valor no registro da conta
+			// em vez de o documento inteiro.
 			{$set: {password:hashedPassword}}, 
+			// A definição de upsert como false significa que a consulta vai 
+			// funcionar apenas em um documento que exista no banco de dados;
+			// ela não vai criar uma conta nova.
 			{upsert:false},
 			function changePasswordCallback(err) {
 				console.log('Change password done for account ' + accountId);
@@ -40,5 +45,76 @@ module.exports = function(config, mongoose, nodemailer) {
 		);
 	};
 
-}
+	// A função forgotPassword envia um e-mail para o proprietário da conta
+	// instruindo-o a redefinir a senha
+	var forgotPassword = function(email, resetPasswordUrl, callback) {
+		var user = Account.findOne( {email: email}, function findAccount(err, doc) {
+			if (err) {
+				// Endereço de email não é um usuário válido
+				callback(false);
+			} else {
+				var smtpTransport = nodemailer.createTransport('SMTP', config.mail);
+				resetPasswordUrl += '?account=' + doc._id;
+				smtpTransport.sendMail({
+					from: 'thisapp@example.com',
+					to: doc.email,
+					subject: 'SocialNet Password Request',
+					text: 'Click here to reset your password: ' + resetPasswordUrl
+				}, function forgotPasswordResult(err) {
+					if (err) {
+						callback(false);
+					} else {
+						callback(true);
+					}
+				});
+			}
+		});
+	};
+
+	//
+	// As funções login e register usam conceitos de 'digest'.
+	// Um bom artigo para entender esse conceito é esse:
+	// http://www.unixwiz.net/techtips/iguide-crypto-hashes.html
+	//
+	//
+	// "A função de login consulta o MongoDB e retorna um sinalizador (flag)
+	// de verdadeiro ou falso indicando se foi ou não capaz de encontrar um usuário cujo
+	// endereço de e-mail e senha hasheada correspondem às credenciais de login
+	// fornecidas pelo Node.js. Se nenhuma conta tiver sido encontrada no banco de dados
+	// a variável doc será nula; do contrário, ela será preenchida a partir do MongoDB.
+	//
+	var login = function(email, password, callback) {
+		var shaSum = crypto.createHash('sha256'); // cria um hash criptográfico usando o algoritmo 'sha256'
+		shaSum.update(password); // updates the hash content with the given data (i.e. password)
+		Account.findOne({email:email, password:shaSum.digest('hex')}, function(err, doc) {
+			callback(null!=doc);
+		});
+	};
+
+	var register = function(email, password, firstName, lastName) {
+		var shaSum = crypto.createHash('sha256'); // cria um hash criptográfico usando o algoritmo 'sha256'
+		shaSum.update(password); // updates the hash content with the given data (i.e. password)
+
+		console.log('Registering ' + email);
+		var user = new Account({
+			email: email,
+			name: {
+				first: firstName,
+				last: lastName
+			},
+			password: shaSum.digest('hex') // essa operação é: senha -> hash() -> digest, e o digest é armazenado
+		});
+		user.save(registerCallback);
+		console.log('Save command was sent');
+	};
+
+	return {
+		register: register,
+		forgotPassword: forgotPassword,
+		changePassword: changePassword,
+		login: login,
+		Account: Account
+	}
+
+} // fim
 
