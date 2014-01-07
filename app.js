dfda4f55e8d323b916b9032db339884df62fbfb5
Paulo Santos Abreu <abreups@gmail.com>
2014-01-07 15:51:41 -0200

    ateh pagina 82

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.get('/', function(req, res){
 	res.render("index.jade", {layout:false});
 });
 
+app.get('/account/authenticated', function(req, res) {
+	if ( req.session.loggedIn ) {
+		res.send(200); // 200 == OK
+	} else {
+		res.send(401); // 401 == not authorized
+	}
+});
+
+
+
 app.listen(8080);
 console.log('Escutando na porta 8080');
 
