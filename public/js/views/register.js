e92d07f0c64cf4595529bcfe7f45a541a2d78a8c
Paulo Santos Abreu <abreups@gmail.com>
2014-01-08 17:26:32 -0200

    arquivos register acrescentados

---
+++ public/js/views/register.js
@@ -0,0 +1,30 @@
+define(['text!templates/register.html'], function(registerTemplate) {
+	var registerView = Backbone.View.extend({
+		el: $('#content');
+
+		events: {
+			"submit form": "register"
+		},
+
+		register: function() {
+			$.post('/register', {
+				firstName: $('input[name=firstName]').val(),
+				lastName: $('input[name=lastName]').val(),
+				email: $('input[name=password]').val(),
+				password: $('input[name=password]').val()
+			}, function(data) {
+				console.log(data);
+			});
+			return false;
+		},
+
+		render:function() {
+			this.$el.html(registerTemplate);
+		}
+
+	});
+
+	return registerView;
+
+});
+
