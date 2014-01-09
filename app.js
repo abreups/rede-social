2630db4eabb91a0533e9761e8cf4f499ccee23cc
Paulo Santos Abreu <abreups@gmail.com>
2014-01-09 17:04:18 -0200

    cap 6 soh faltalayout.jade e rever app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.configure(function() {
 	app.use(express.static(__dirname + '/public'));
 });
 
+// Trata acesso à página raiz do site
 app.get('/', function(req, res){
 	res.render("index.jade", {layout:false});
 });
@@ -18,6 +19,77 @@ app.get('/account/authenticated', function(req, res) {
 	}
 });
 
+// Trata acesso à página de registro
+app.post('/register', function(req, res) {
+	var firstName = req.param('firstName', '');
+	var lastName = req.param('lastName', '');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+
+	if ( null == email || null == password)  {
+		res.send(400); // 400 == bad request / solicitação incorreta
+		return;
+	}
+	Account.register(email, password, firstName, lastName);
+	res.send(200); // Atenção: enviado sem que se saiba se a ação anterior teve sucesso
+});
+
+// Trata acesso à página de login
+app.post('/login', function(req, res) {
+	console.log('login request');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+	if ( null == email || email.length < 1 || null == password || password.length < 1) {
+		res.send(400); // 400 == bad request / solicitação incorreta
+		return;
+	}
+
+	Account.login(email, password, function(success) {
+		if ( !success ) {
+			res.send(401);
+			return;
+		}
+		console.log('login was successful');
+		res.send(200);
+	});
+});
+
+// Trata acesso à página de senha esquecida
+app.post('/forgotpassword', function(req, res) {
+	var hostname = req.headers.host;
+	// Prepara a URL a ser usada no reset da senha
+	var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
+	var email = req.param('email', null);
+	if ( null == email || email.length < 1 ) {
+		res.send(400);
+		return;
+	}
+
+	Account.forgotPassword(email, resetPasswordUrl, function(success) {
+		if (success) {
+			res.send(200);
+		} else {
+			// Nome de usuário ou senha não encontrado
+			res.send(404);
+		}
+	});
+});
+
+// Trata reset de senha
+// 1. GET
+app.get('/resetPassword', function(req, res) {
+	var accountId = req.param('account', null);
+	res.render('resetPassword.jade', {locals:{accountId:accountId}});
+});
+// 2. POST
+app.post('/resetPassword', function(req, res) {
+	var accountId = req.param('accountId', null);
+	var password = req.param('password', null);
+	if ( null != accountId && null != password ) {
+		Account.changePassword(accountId, password);
+	}
+	res.render('resetPasswordSuccess.jade');
+});
 
 
 app.listen(8080);
